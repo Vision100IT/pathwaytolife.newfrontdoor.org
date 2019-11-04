ee2d58369b62dce5175a0c5b08e53f1b7916a78a
Bratalix <bratalix@gmail.com>
2019-11-04 17:48:30 +1100

    Camp notice

--- src/components/homepage/SocialBlock.js
+++ src/components/homepage/SocialBlock.js
@@ -13,6 +13,9 @@ class SocialBlock extends Component {
 
                         <div className="content">
                             <div className="upcoming-events">
+                                <p>
+                                    <strong>Please note that this Sunday the 10th of November our Sunday Service will be off-site at our <a href="https://www.facebook.com/events/341010700185412/" target="_blank" rel="noreferrer noopener">Church Camp</a> at <a href="https://goo.gl/maps/PxXXMhM3FdyW75y5A" target="_blank" rel="noreferrer noopener">Paton Park</a> at 11am. All welcome.</strong><br />
+                                </p>
                                 <p>The best place to go to find out what's coming up and happening at Pathway is the Church <a href="https://www.facebook.com/PathwaytoLifeChurch/" target="_blank" rel="noreferrer noopener">Facebook page</a>.</p>
                             </div>
                         </div>
